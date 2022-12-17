a1eb1aca54962986c4894c03d62da1217dfa8050
warfa@LAPTOP-BGKII3AT <warfacesdr@mail.ru>
2022-12-17 23:04:30 +0800

    Задача: Принимает число , выдаёт квадрат

--- Program.cs
+++ Program.cs
@@ -1,7 +1,13 @@
-﻿Console.WriteLine("Введите первое число");
-int NumberA = Console.ReadLine();
-Console.WriteLine("Введите первое число");
-int NumberB = Console.ReadLine();
+﻿// Напишите программу,которая на вход принимает число и выдаёт его квадрат
+// Например:
+// 4 -> 16
+// 9 -> 81
+Console.WriteLine("Введите число");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+// Так как по умалчанию в C# всё что мы вводим в Console.ReadLine для терминала является данными строки(string)
+//Поэтому нужно крнвертировать данные из string в int (int number = Convert.ToInt32(Console.ReadLine);
+int result = NumberA * NumberA;
+System.Console.WriteLine("Квадрат числа " + NumberA + " будет " + result);
 
 
 
